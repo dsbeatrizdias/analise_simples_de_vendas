ea32c08261c7b49997dba9d74858faf140bfaa13
Beatriz Dias <beatrizdias.ds@outlook.com>
2026-04-22 15:35:15 -0300

    analise.py

--- fontes/analise.py
+++ fontes/analise.py
@@ -1,1 +1,18 @@
-print("Projeto de análise de vendas")
+import csv
+
+valores = []
+
+with open('../dados/vendas.csv', newline='', encoding='utf-8') as arquivo:
+    leitor = csv.DictReader(arquivo)
+    
+    for linha in leitor:
+        valores.append(float(linha['valor']))
+
+total = sum(valores)
+media = total / len(valores)
+maior = max(valores)
+
+print("📊 RELATÓRIO DE VENDAS")
+print(f"Total de vendas: R$ {total}")
+print(f"Média de vendas: R$ {media:.2f}")
+print(f"Maior venda: R$ {maior}")
